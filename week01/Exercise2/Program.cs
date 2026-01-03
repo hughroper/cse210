ec43ccbcf6833fb2503a32396bab37e0ae203629
hughroper <hughroper@icloud.com>
2026-01-02 18:00:02 -0700

    Week 1 Execise 2 complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.IO.Pipelines;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+        string letter = "";
+        float gradePlusOrMinus = gradePercentage % 10;
+        string PlusOrMinus = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage < 90 && gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage < 80 && gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage < 70 && gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+
+        if (gradePlusOrMinus >= 7)
+        {
+            PlusOrMinus = "+";
+        }
+        else if (gradePlusOrMinus <= 3)
+        {
+            PlusOrMinus = "-";
+        }
+
+        if (gradePercentage >= 93 || gradePercentage < 60)
+        {
+            Console.WriteLine($"Your grade is: {letter}");            
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is: {letter}{PlusOrMinus}");            
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else if (gradePercentage < 70)
+        {
+            Console.WriteLine("Sorry, you didn't pass the class, maybe next time.");
+        }
+
+
     }
 }
