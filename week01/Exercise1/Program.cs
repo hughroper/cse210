8dcf1f1d3931fe8be9cd5f462e37f3804f8697c2
hughroper <hughroper@icloud.com>
2026-05-03 15:36:42 -0600

    W01 Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("\nWhat is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.\n");
+
     }
 }
