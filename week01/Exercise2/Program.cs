a1cf549ef349e1fe6188e0abff5c093589f1baca
hughroper <hughroper@icloud.com>
2026-05-03 17:56:59 -0600

    W1 X2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("\nEnter your grade percentage: ");
+        string inputValue = Console.ReadLine();
+        float gradePercent = float.Parse(inputValue);
+        string letter = "";
+
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"\nYour grade is a {letter}.\n");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the course.\n");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you didn't pass, better luck next time.\n");
+        }
+
+
+
     }
 }
