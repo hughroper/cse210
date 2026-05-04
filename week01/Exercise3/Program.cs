22916292cb79184647dbe33403067ec86bb17b1f
hughroper <hughroper@icloud.com>
2026-05-03 18:17:41 -0600

    Magic Number version 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("\nWhat is the magic number?");
+        string magicInput = Console.ReadLine();
+        int magicNumber = int.Parse(magicInput);
+
+        Console.Write("\nWhat is your guess?");
+        string guessInput = Console.ReadLine();
+        int guessNumber = int.Parse(guessInput);
+
+        if (magicNumber > guessNumber)
+        {
+            Console.WriteLine("\nHigher");
+        }
+        else if (magicNumber < guessNumber)
+        {
+            Console.WriteLine("\nLower");
+        }
+        else
+        {
+            Console.WriteLine("\nYou guessed it!\n");
+        }
+
     }
 }
