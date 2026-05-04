ef2979598f0003a24c149dd290b06649dd916f17
hughroper <hughroper@icloud.com>
2026-05-03 18:25:37 -0600

    W1 X2 Version 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,25 +7,35 @@ class Program
 
         // Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
-        Console.Write("\nWhat is the magic number?");
-        string magicInput = Console.ReadLine();
-        int magicNumber = int.Parse(magicInput);
+        // Console.Write("\nWhat is the magic number?");
+        // string magicInput = Console.ReadLine();
+        // int magicNumber = int.Parse(magicInput);
 
-        Console.Write("\nWhat is your guess?");
-        string guessInput = Console.ReadLine();
-        int guessNumber = int.Parse(guessInput);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 11);
 
-        if (magicNumber > guessNumber)
+        int guessNumber = -1;
+
+        while (magicNumber != guessNumber)
         {
-            Console.WriteLine("\nHigher");
-        }
-        else if (magicNumber < guessNumber)
-        {
-            Console.WriteLine("\nLower");
-        }
-        else
-        {
-            Console.WriteLine("\nYou guessed it!\n");
+
+            Console.Write("\nWhat is your guess?");
+            string guessInput = Console.ReadLine();
+            guessNumber = int.Parse(guessInput);
+
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("\nHigher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("\nLower");
+            }
+            else
+            {
+                Console.WriteLine("\nYou guessed it!\n");
+            }
+
         }
 
     }
