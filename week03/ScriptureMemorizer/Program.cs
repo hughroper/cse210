b07d487f2a2c5168e45f8bf0a4bee0376bc98a85
hughroper <hughroper@icloud.com>
2026-05-27 20:31:49 -0600

    scripture update

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,33 +5,6 @@ class Program
     static void Main(string[] args)
     {
 
-        // Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
-
-        // Word word1 = new Word("Faith");
-
-        // Console.WriteLine(word1.GetDisplayText());
-
-        // word1.Hide();
-
-        // Console.WriteLine(word1.GetDisplayText());
-
-        // Reference ref1 = new Reference("John", 3, 16);
-        // Console.WriteLine(ref1.GetDisplayText());
-        // Reference ref2 = new Reference("Proverbs", 3, 5, 6);
-        // Console.WriteLine(ref2.GetDisplayText());
-
-        // Reference ref1 = new Reference("John", 3, 16);
-        // Scripture scripture = new Scripture(
-        //     ref1, "For God so loved the world"
-        // );
-        // Console.WriteLine(scripture.GetDisplayText());
-        // scripture.HideRandomWords(2);
-        // Console.WriteLine(scripture.GetDisplayText());
-
-        // Reference ref1 = new Reference("John", 3, 16);
-        // Scripture scripture = new Scripture(
-        //     ref1, "For God so loved the world"
-        // );
         Reference ref2 = new Reference("Proverbs", 3, 5, 6);
         Scripture scripture = new Scripture(
             ref2, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths."
