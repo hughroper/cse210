ae71389f13936a8f40509ce0a1c554c3bb24d72b
hughroper <hughroper@icloud.com>
2026-05-29 17:43:59 -0600

    W4 Start to video

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        // Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+        Video video1 = new Video(
+            "How to Use a Mesh Radio",
+            "Hugh Roper",
+            915
+        );
+
+        Comment c1 = new Comment(
+           "Joe",
+          "Great introduction to MeshCore!"
+        );
+
+        Comment c2 = new Comment(
+            "Sue",
+            "This helped me get my Heltec V4 working."
+        );
+
+        Comment c3 = new Comment(
+            "Bob",
+            "Can you make a video about room servers?"
+        );
+
+        video1.AddComment(c1);
+        video1.AddComment(c2);
+        video1.AddComment(c3);
+
+        video1.DisplayVideoInfo();
+
+
     }
 }
