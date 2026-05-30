270d92dea0204ada6d553f47548b7927ea2a06b8
hughroper <hughroper@icloud.com>
2026-05-29 18:45:43 -0600

    Online orders

---
+++ week04/OnlineOrdering/Customer.cs
@@ -0,0 +1,7 @@
+public class Customer
+{
+
+    private string _name;
+    private Address _address;
+
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+
+
+
     }
 }
