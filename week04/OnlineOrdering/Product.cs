cf702df16997bb207b0356678d313fbb875ecb0c
hughroper <hughroper@icloud.com>
2026-05-29 18:51:07 -0600

    ID

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -2,19 +2,28 @@ public class Product
 {
 
     private string _name;
-    private string _productId;
+    private string _productID;
     private double _price;
     private int _quantity;
 
 
 
-    public Product(string name, string productId, double price, int quantity)
+    public Product(string name, string productID, double price, int quantity)
     {
         _name = name;
-        _productId = productId;
+        _productID = productID;
         _price = price;
         _quantity = quantity;
     }
 
+    public double GetTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetPackingInfo()
+    {
+        return $"{_name} ({_productID})";
+    }
 
 }
